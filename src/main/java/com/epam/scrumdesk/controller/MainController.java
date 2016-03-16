f0c90a9101254db588d83831cd11e8cf35e9d71c
Meirzhan_Rymbayev <meirzhan_rumbaev@mail.ru>
2016-03-16 17:52:06 +0600

    [>>>] Work to be continuous...

--- src/main/java/com/epam/scrumdesk/controller/MainController.java
+++ src/main/java/com/epam/scrumdesk/controller/MainController.java
@@ -1,10 +1,13 @@
 package com.epam.scrumdesk.controller;
 
 import com.epam.scrumdesk.model.Task;
+import com.epam.scrumdesk.service.TaskService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -15,8 +18,20 @@ public class MainController {
     TaskService taskService;
 
     @RequestMapping(value = "/task/{id}", method = RequestMethod.PUT)
-    public ResponseEntity<Task> updateTask(@PathVariable("id") long id) {
+    public ResponseEntity<Task> updateTask(@PathVariable("id") long id, @RequestBody Task task) {
         Task currentTask = taskService.findById(id);
+
+        if (currentTask == null) {
+            //TODO Logger
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+
+        currentTask.setText(task.getText());
+        currentTask.setStatus(task.getStatus());
+
+
         taskService.updateTask(currentTask);
+
+        return new ResponseEntity<>(currentTask, HttpStatus.OK);
     }
 }
--- src/main/java/com/epam/scrumdesk/service/TaskServiceImpl.java
+++ src/main/java/com/epam/scrumdesk/service/TaskServiceImpl.java
@@ -6,6 +6,8 @@ import com.epam.scrumdesk.model.Task;
  * Created by Meirzhan_Rymbayev on 3/16/2016.
  */
 public class TaskServiceImpl implements TaskService {
+
+
     @Override
     public Task findById(long id) {
         return null;
