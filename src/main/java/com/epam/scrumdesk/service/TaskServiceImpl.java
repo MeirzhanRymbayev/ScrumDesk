5b2d1afe734666b08194c27e163aa396c6f88717
Meirzhan_Rymbayev <meirzhan_rumbaev@mail.ru>
2016-03-16 14:09:34 +0600

    [IMP] [TaskServiceImpl] two methods was added...

---
+++ src/main/java/com/epam/scrumdesk/service/TaskServiceImpl.java
@@ -0,0 +1,18 @@
+package com.epam.scrumdesk.service;
+
+import com.epam.scrumdesk.model.Task;
+
+/**
+ * Created by Meirzhan_Rymbayev on 3/16/2016.
+ */
+public class TaskServiceImpl implements TaskService {
+    @Override
+    public Task findById(long id) {
+        return null;
+    }
+
+    @Override
+    public void updateTask(Task task) {
+
+    }
+}
